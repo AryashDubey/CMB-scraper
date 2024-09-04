a1bfb83b8c7695c86d685246629cc49ba5f2415c
Aryash Dubey <dubeyaryash@gmail.com>
2024-09-03 20:54:16 -0700

    server fix

--- scraper.js
+++ scraper.js
@@ -28,13 +28,7 @@ async function searchProduct(query) {
   const browser = await puppeteer.launch({
     headless: true,
 
-    args: [`--proxy-server=${randomProxy}`,
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--disable-gpu',
-    ],
+    args: [`--proxy-server=${randomProxy}`, "--no-sandbox"],
   });
 
   const [page] = await browser.pages();
@@ -46,39 +40,43 @@ async function searchProduct(query) {
   const width = 1024 + Math.floor(Math.random() * 100);
   const height = 768 + Math.floor(Math.random() * 100);
   await page.setViewport({ width, height });
+  console.log(randomProxy);
 
-  // Add additional headers
-  await page.setExtraHTTPHeaders({
-    "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
-  });
+  try {
+    // Implement rate limiting
+    await page.goto(
+      `https://www.homedepot.com/s/${encodeURIComponent(query)}`,
+      {
+        waitUntil: "networkidle0",
+        timeout: 60000,
+      }
+    );
 
+    await page.waitForSelector('[data-testid="product-pod"]');
 
-  try {  
-  // Implement rate limiting
-  await page.goto(`https://www.homedepot.com/s/${encodeURIComponent(query)}`, {
-    waitUntil: "networkidle0",
-    timeout: 60000,
-  });
-  
-  await page.waitForSelector('[data-testid="product-pod"]');
+    // Extract product information
+    const products = await page.evaluate(() => {
+      return Array.from(
+        document.querySelectorAll('[data-testid="product-pod"]')
+      ).map((product) => {
+        const nameElement = product.querySelector(
+          '[data-testid="product-header"] a'
+        );
+        const priceElement = product.querySelector(
+          ".price__wrapper .price-format__main-price"
+        );
 
+        const name = nameElement
+          ? nameElement.innerText.trim()
+          : "No name available";
+        const link = nameElement ? nameElement.href : "No link available";
+        const price = priceElement
+          ? `${priceElement.textContent.trim()}`
+          : "Price not available";
 
-      await page.waitForSelector('[data-testid="product-pod"]');
-
-      // Extract product information
-      const products = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('[data-testid="product-pod"]')).map(product => {
-            const nameElement = product.querySelector('[data-testid="product-header"] a');
-            const priceElement = product.querySelector('.price__wrapper .price-format__main-price');
-
-            const name = nameElement ? nameElement.innerText.trim() : 'No name available';
-            const link = nameElement ? nameElement.href : 'No link available';
-            const price = priceElement ? `${priceElement.textContent.trim()}` : 'Price not available';
-
-            return { name, link, price };
-        });
+        return { name, link, price };
       });
+    });
 
     await browser.close();
     return products;
